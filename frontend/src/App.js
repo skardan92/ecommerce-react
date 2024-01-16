84846365c1b2960572ba903da2c13b16a2383d2a
Daniel <ds@MacBook-Air-DS.local>
2024-01-16 18:26:51 +0100

    category and login signup components added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ import ShopCategory from "./Pages/ShopCategory";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
 import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
 
 
 function App() {
@@ -17,9 +20,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory category="mens"/>}/>
-        <Route path='/womens' element={<ShopCategory category="women"/>}/>
-        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/mens' element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>}/>
         <Route path="/product" element={<Product/>}>
           <Route path=':productId' element={<Product/>}/>
         </Route>
--- frontend/src/pages/product.jsx
+++ frontend/src/pages/product.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { ShopContext } from '../Context/ShopContext'
 
 const Product = () => {
+  const {all_product}= useContext(ShopContext);
   return(
     <div>
 
